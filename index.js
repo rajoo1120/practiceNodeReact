a42efdf3b1e055b5b86587a800bb32d806417656
heejin <dongdong7251@naver.com>
2022-02-06 20:51:10 +0900

    key protection

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key');
+
+const { User } = require("./models/User")
+const bodyParser = require('body-parser')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+//application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hee:abcd1234@bolierplate.8fpim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(()=>console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕')
+})
+
+app.post('/register',(req,res)=>{
+  //회원가입에 필요한 정보들을 client에서 가져오면
+  //그것을 DB에 넣어줌 
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success : true
+    })
+  })
 })
 
 app.listen(port, () => {
